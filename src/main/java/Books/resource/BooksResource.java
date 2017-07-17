fe205e4aef36bc62798642bc52472ca9b7f8cc9c
ricky-clevinger <ricky.clevinger@cgi.com>
2017-07-17 11:17:54 -0400

    Spacing

--- src/main/java/Books/resource/BooksResource.java
+++ src/main/java/Books/resource/BooksResource.java
@@ -4,7 +4,6 @@ import Books.model.Book;
 import Books.repository.BooksRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
