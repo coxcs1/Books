c6b798de462289e0b6afe0e96b99707db011559e
ricky-clevinger <ricky.clevinger@cgi.com>
2017-07-25 14:45:15 -0400

    Due Date

--- src/main/java/Books/resource/BooksResource.java
+++ src/main/java/Books/resource/BooksResource.java
@@ -117,7 +117,7 @@ public class BooksResource {
                            @PathVariable(value = "mid") int mid,
                            @PathVariable(value = "check") int check){
         Books b = bookRepository.findOne(titleId);
-        Date date = new Date(System.currentTimeMillis());
+        Date date = new Date(System.currentTimeMillis() + (1000*60*60*24*7) );
         b.setCheck(check);
         b.setMid(mid);
         if (mid == 0){
