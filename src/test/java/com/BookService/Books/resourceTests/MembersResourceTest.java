a2930bf7c1106d69794b9ff00d0be4b7ae32af0b
ricky-clevinger <ricky.clevinger@cgi.com>
2017-08-01 10:09:40 -0400

    Unit Tests
    2 new unit tests to MembersResourceTests

--- src/test/java/com/BookService/Books/resourceTests/MembersResourceTest.java
+++ src/test/java/com/BookService/Books/resourceTests/MembersResourceTest.java
@@ -20,6 +20,8 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static java.util.Collections.emptyList;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = MembersResource.class, secure = false)
@@ -59,7 +61,7 @@ public class MembersResourceTest {
 
 
     @Test
-    public void getByIdTest() throws Exception {
+    public void getByValidIdTest() throws Exception {
 
         // Creates a list of one to return of specific element in mockFullList
         mockMem = Collections.singletonList(mockFullList.get(1));
@@ -81,11 +83,36 @@ public class MembersResourceTest {
 
         //Compares expected result with the actual result.
         JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
-    }//end getByIdTest
+    }//end getByValidIdTest
+
+    @Test
+    public void getByInValidIdTest() throws Exception {
+
+        // Creates a list of one to return of specific element in mockFullList
+        mockMem = emptyList();
+
+        //Returns mockFullList when getAll is called.
+        Mockito.when(
+                membersResource.getById(Integer.MAX_VALUE)).thenReturn(mockMem);
+
+        //Builds the request
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+                "/members/id/" + Integer.MAX_VALUE).accept(
+                MediaType.APPLICATION_JSON);
+
+        //Sets the result
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        //What the expected result is
+        String expected = "[]";
+
+        //Compares expected result with the actual result.
+        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+    }//end getByInValidIdTest
 
 
     @Test
-    public void getByFNameTest() throws Exception {
+    public void getByValidFNameTest() throws Exception {
 
         // Creates a list of one to return of specific element in mockFullList
         mockMem = Collections.singletonList(mockFullList.get(2));
@@ -107,7 +134,32 @@ public class MembersResourceTest {
 
         //Compares expected result with the actual result.
         JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
-    }//end getByFNameTest
+    }//end getByValidFNameTest
+
+    @Test
+    public void getByInValidFNameTest() throws Exception {
+
+        // Creates a list of one to return of specific element in mockFullList
+        mockMem = emptyList();
+
+        //Returns mockFullList when getAll is called.
+        Mockito.when(
+                membersResource.getByFName("455gre3")).thenReturn(mockMem);
+
+        //Builds the request
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(
+                "/members/fname/455gre3").accept(
+                MediaType.APPLICATION_JSON);
+
+        //Sets the result
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        //What the expected result is
+        String expected = "[]";
+
+        //Compares expected result with the actual result.
+        JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+    }//end getByInValidFNameTest
 
 
     @Test
